3b457c374efbd701c34a857aa538d61bd2a71c4d
Ehssan Hoorvash <ehssan.hoorvash@widespace.com>
2015-10-28 10:31:42 +0100

    Implemented Gateway and channel

--- src/main/java/com/widespace/wisper/route/Channel.java
+++ src/main/java/com/widespace/wisper/route/Channel.java
@@ -1,8 +1,43 @@
 package com.widespace.wisper.route;
 
-/**
- * Created by ehssanhoorvash on 27/10/15.
- */
-public class Channel
+import com.widespace.wisper.base.WisperObject;
+import com.widespace.wisper.classrepresentation.RPCClass;
+import com.widespace.wisper.controller.Gateway;
+import com.widespace.wisper.controller.GatewayCallback;
+import com.widespace.wisper.messagetype.AbstractMessage;
+
+
+public abstract class Channel extends WisperObject implements GatewayCallback
 {
+    Gateway gateway;
+
+    public static RPCClass registerRpcClass()
+    {
+        return null;
+    }
+
+    public Gateway getGateway()
+    {
+        return gateway;
+    }
+
+    public void setGateway(Gateway gateway)
+    {
+        this.gateway = gateway;
+    }
+
+    @Override
+    public void gatewayReceivedMessage(AbstractMessage message)
+    {
+       // Do Nothing
+    }
+
+    @Override
+    public void gatewayGeneratedMessage(String message)
+    {
+       sendMessage(message);
+    }
+
+    protected abstract void sendMessage(String message);
+    protected abstract void receiveMessage(String message);
 }
