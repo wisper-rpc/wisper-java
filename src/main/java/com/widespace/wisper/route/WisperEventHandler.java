cd1310937e2b8792da05f023cc2ee076ebb0bafc
Ehssan Hoorvash <ehssan.hoorvash@widespace.com>
2016-03-01 16:08:46 +0100

    WSAF-3578 WSAF-3119 Started WisperEventHandler throw exception if message is not notifictaion

--- src/main/java/com/widespace/wisper/route/WisperEventHandler.java
+++ src/main/java/com/widespace/wisper/route/WisperEventHandler.java
@@ -2,6 +2,7 @@ package com.widespace.wisper.route;
 
 import com.widespace.wisper.classrepresentation.WisperClassModel;
 import com.widespace.wisper.messagetype.AbstractMessage;
+import com.widespace.wisper.messagetype.Notification;
 import com.widespace.wisper.messagetype.error.WisperException;
 import org.jetbrains.annotations.NotNull;
 
@@ -15,9 +16,12 @@ public class WisperEventHandler
 
     public WisperEventHandler(@NotNull Router router, @NotNull WisperClassModel wisperClassModel, @NotNull AbstractMessage message)
     {
+        if (! (message instanceof Notification))
+            throw new WisperException(UNEXPECTED_TYPE_ERROR, null, "Remote instance event handler was called with a non-Notification message type.");
+
         WisperCallType callType = MessageParser.getCallType(message);
         if (callType != WisperCallType.STATIC_EVENT && callType != WisperCallType.INSTANCE_EVENT)
-            throw new WisperException(UNEXPECTED_TYPE_ERROR, null, "Remote instance event handler was called with a non-Event message type.");
+            throw new WisperException(UNEXPECTED_TYPE_ERROR, null, "Remote instance event handler was called with a non-Event message call type.");
 
         this.router = router;
         this.wisperClassModel = wisperClassModel;
--- src/test/java/com/widespace/wisper/route/WisperEventHandlerTest.java
+++ src/test/java/com/widespace/wisper/route/WisperEventHandlerTest.java
@@ -2,6 +2,7 @@ package com.widespace.wisper.route;
 
 import com.widespace.wisper.classrepresentation.WisperClassModel;
 import com.widespace.wisper.messagetype.Notification;
+import com.widespace.wisper.messagetype.Request;
 import com.widespace.wisper.messagetype.error.WisperException;
 import org.junit.Test;
 
@@ -16,5 +17,19 @@ public class WisperEventHandlerTest
         Notification nonEventMessage = new Notification();
         nonEventMessage.setMethodName("a.b.c:d");
         new WisperEventHandler(mock(Router.class), mock(WisperClassModel.class), nonEventMessage);
+    }
+
+    @Test(expected = WisperException.class)
+    public void givenNonNotificationMessage_ShouldThrowException() throws Exception
+    {
+        Request nonNotif = new Request().withMethodName("a.b.a!");
+        new WisperEventHandler(mock(Router.class), mock(WisperClassModel.class), nonNotif);
+    }
+
+    @Test
+    public void givenStaticEventType_CallsOnClass() throws Exception
+    {
+
+
     }
 }
