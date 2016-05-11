774642ebc7a8bbdad6d073a99a5c4eaa11147a00
Patrik Nyblad <patrik@Patrik-Nyblads-MacBook-Pro.local>
2016-05-11 17:11:46 +0200

    Add unit tests for create, destroy, register with event router and unregister with event router

--- src/test/java/com/widespace/wisper/base/WisperRemoteObjectTest.java
+++ src/test/java/com/widespace/wisper/base/WisperRemoteObjectTest.java
@@ -25,7 +25,8 @@ public class WisperRemoteObjectTest
     @Test
     public void testInstanceIdentifier() throws Exception
     {
-        WisperRemoteObject obj = new MyWisperRemoteObject("Foo", mock(GatewayRouter.class));
+        StubbedGatewayRouter router = new StubbedGatewayRouter();
+        WisperRemoteObject obj = new MyWisperRemoteObject("Foo", router);
 
         assertThat(obj.getInstanceIdentifier(), is(equalTo(null)));
 
@@ -34,41 +35,106 @@ public class WisperRemoteObjectTest
         assertThat(obj.getInstanceIdentifier(), is(equalTo("foo1")));
     }
 
+    @Test
+    public void testCreateInstanceSendsCreateMessage() throws Exception
+    {
+        StubbedGatewayRouter router = new StubbedGatewayRouter();
+        WisperRemoteObject obj = new MyWisperRemoteObject("Foo", router);;
+
+        // Assert that only create message has been sent
+        assertThat(router.messages.size(), is(equalTo(1)));
+
+        Request createRequest = (Request)router.messages.get(0);
+
+        assertThat(createRequest.getMethodName(), is(equalTo("Foo~")));
+        assertThat(createRequest.getParams(), is(equalTo(null)));
+    }
+
+    @Test
+    public void testCreateInstanceResponseRegistersInstanceWithEventRouter() throws Exception
+    {
+        StubbedGatewayRouter router = new StubbedGatewayRouter();
+        WisperRemoteObject obj = new MyWisperRemoteObject("Foo", router);;
+
+        obj.setInstanceIdentifier("mockedInstanceId0x00");
+
+        EventRouter eventRouter = (EventRouter) router.getRoutes().get("Foo");
+
+        assertThat(eventRouter.getRemoteObjects().size(), is(1));
+        assertThat(eventRouter.getRemoteObjects().get("mockedInstanceId0x00"), is(equalTo(obj)));
+    }
+
+    @Test
+    public void testDestroyInstanceSendsDestroyMessage() throws Exception
+    {
+        StubbedGatewayRouter router = new StubbedGatewayRouter();
+        WisperRemoteObject obj = new MyWisperRemoteObject("Foo", router);;
+
+        obj.setInstanceIdentifier("mockedInstanceId0x00");
+        obj.destroy();
+
+        // Assert that only create message has been sent
+        assertThat(router.messages.size(), is(equalTo(2)));
+
+        Notification destroyNotification = (Notification) router.messages.get(1);
+
+        System.out.println("ASSDASDASD");
+        System.out.println(destroyNotification.getMethodName());
+
+        assertThat(destroyNotification.getMethodName(), is(equalTo("Foo:~")));
+        assertThat(destroyNotification.getParams().length, is(1));
+        assertThat((String)destroyNotification.getParams()[0], is(equalTo("mockedInstanceId0x00")));
+    }
+
+    @Test
+    public void testDestroyInstanceUnregistersInstanceFromEventRouter() throws Exception
+    {
+        StubbedGatewayRouter router = new StubbedGatewayRouter();
+        WisperRemoteObject obj = new MyWisperRemoteObject("Foo", router);;
+
+        obj.setInstanceIdentifier("mockedInstanceId0x00");
+        obj.destroy();
+
+        EventRouter eventRouter = (EventRouter) router.getRoutes().get("Foo");
+
+        assertThat(eventRouter.getRemoteObjects().size(), is(0));
+    }
 
     @Test
     public void testCallInstanceMethod() throws Exception
     {
-        StubbedGatewayRouter channel = new StubbedGatewayRouter();
-        WisperRemoteObject obj = new MyWisperRemoteObject("Foo", channel);
+        StubbedGatewayRouter router = new StubbedGatewayRouter();
+        WisperRemoteObject obj = new MyWisperRemoteObject("Foo", router);
 
         // Call a method before instance identifier has been set
         obj.callInstanceMethod("bar", new Object[]{1, 2, 3});
 
-        // Assert that no message has been sent
-        assertThat(channel.messages.size(), is(equalTo(0)));
+        // Assert that only create message has been sent
+        assertThat(router.messages.size(), is(equalTo(1)));
 
-        // Set the identifier
+        // Simulate a create response by setting the instance identifier
         obj.setInstanceIdentifier("foo2");
 
-        Request expected = new Request("Foo:bar", new Object[]{"foo2", 1, 2, 3}).withResponseBlock(WisperRemoteObject.DoNothingResponseBlock);
+        Notification expected = new Notification("Foo:bar", new Object[]{"foo2", 1, 2, 3});
 
         // Assert a message has been sent and that it equals the expected one
-        assertThat(channel.messages.size(), is(equalTo(1)));
-        assertThat((Request) channel.messages.get(0), is(equalTo(expected)));
+        assertThat(router.messages.size(), is(equalTo(2)));
+        assertThat((Notification) router.messages.get(1), is(equalTo(expected)));
 
         // Call the method a second time
         obj.callInstanceMethod("bar", new Object[]{1, 2, 3});
 
+
         // Assert that another message is sent immediately
-        assertThat(channel.messages.size(), is(equalTo(2)));
-        assertThat((Request) channel.messages.get(1), is(equalTo(expected)));
+        assertThat(router.messages.size(), is(equalTo(3)));
+        assertThat((Notification) router.messages.get(2), is(equalTo(expected)));
     }
 
     @Test
     public void testCallStaticMethod() throws Exception
     {
-        StubbedGatewayRouter channel = new StubbedGatewayRouter();
-        WisperRemoteObject obj = new MyWisperRemoteObject("Foo", channel);
+        StubbedGatewayRouter gateway = new StubbedGatewayRouter();
+        WisperRemoteObject obj = new MyWisperRemoteObject("Foo", gateway);
 
         obj.callStaticMethod("foo", new Object[]{1});
 
@@ -81,13 +147,9 @@ public class WisperRemoteObjectTest
             }
         });
 
-        for (AbstractMessage message : channel.messages)
-        {
-            Request request = (Request) message;
-
-            // Just call the response block to see that we get complete code coverage
-            request.getResponseBlock().perform(new Response(), null);
-        }
+        // First messages is create, second message is Notification because no response block, third is Request
+        // Just call the response block to see that we get complete code coverage
+        ((Request)gateway.messages.get(2)).getResponseBlock().perform(new Response(), null);
     }
 
     @Test
@@ -98,21 +160,21 @@ public class WisperRemoteObjectTest
 
         obj.sendInstanceEvent("baz", 1);
 
-        // Assert that no message has been sent
-        assertThat(channel.messages.size(), is(equalTo(0)));
+        // Assert that only create message has been sent
+        assertThat(channel.messages.size(), is(equalTo(1)));
 
         // Set the identifier
         obj.setInstanceIdentifier("foo2");
 
         Event expected = new Event("Foo:!", "foo2", "baz", 1);
 
-        assertThat(channel.messages.size(), is(equalTo(1)));
-        assertThat((Event) channel.messages.get(0), is(equalTo(expected)));
+        assertThat(channel.messages.size(), is(equalTo(2)));
+        assertThat((Event) channel.messages.get(1), is(equalTo(expected)));
 
         obj.sendInstanceEvent("baz", 1);
 
-        assertThat(channel.messages.size(), is(equalTo(2)));
-        assertThat((Event) channel.messages.get(1), is(equalTo(expected)));
+        assertThat(channel.messages.size(), is(equalTo(3)));
+        assertThat((Event) channel.messages.get(2), is(equalTo(expected)));
 
     }
 
@@ -125,9 +187,8 @@ public class WisperRemoteObjectTest
         obj.sendStaticEvent("create", null);
 
         Event expected = new Event("Foo!", "create", null);
-
-        assertThat(router.messages.size(), is(equalTo(1)));
-        assertThat((Event) router.messages.get(0), is(equalTo(expected)));
+        assertThat(router.messages.size(), is(equalTo(2)));
+        assertThat((Event) router.messages.get(1), is(equalTo(expected)));
     }
 
 
@@ -169,7 +230,7 @@ public class WisperRemoteObjectTest
         String instanceId = "testIdentifier";
         StubbedGatewayRouter stubbedGatewayRouter = new StubbedGatewayRouter();
         MyWisperRemoteObject remoteObject = new MyWisperRemoteObject("a.b.c",stubbedGatewayRouter);
-        remoteObject.registerInstanceOnEventRouter(instanceId);
+        remoteObject.setInstanceIdentifier(instanceId);
         Event instanceEvent = new WisperEventBuilder().withInstanceIdentifier(instanceId).withMethodName("a.b.c").buildInstanceEvent();
         stubbedGatewayRouter.routeMessage(instanceEvent,"a.b.c");
 
